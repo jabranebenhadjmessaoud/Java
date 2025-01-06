c653332a53ae9abfac895def08e9d36f8e992a6b
Jabrane <jabrane.benhadjmessaoud@gmail.com>
2025-01-06 13:40:05 +0100

    first java program practice

---
+++ Week1/Day1/Practice/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        
+        System.out.println("My name is Jabrane") ;
+        System.out.println("I am 28 years old");
+        System.out.println("My hometown is Djerba, Tunisia");
+    }
+}
