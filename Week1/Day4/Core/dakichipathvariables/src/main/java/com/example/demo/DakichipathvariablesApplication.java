4f13dd05ebcc2515aab5e0dad71af6dbec926462
Jabrane <jabrane.benhadjmessaoud@gmail.com>
2025-01-09 18:37:14 +0100

    daikichipathvariables

---
+++ Week1/Day4/Core/dakichipathvariables/src/main/java/com/example/demo/DakichipathvariablesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DakichipathvariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DakichipathvariablesApplication.class, args);
+	}
+
+}
