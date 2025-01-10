4520924bb86c606a7f4ec91db6fba5b524d89c85
Jabrane <jabrane.benhadjmessaoud@gmail.com>
2025-01-10 16:11:55 +0100

    hooper practice

---
+++ Week1/Day5/Practice/hooper_Receipt/src/main/java/com/hooper/HooperReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.hooper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooperReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooperReceiptApplication.class, args);
+	}
+
+}
---
+++ Week1/Day5/Practice/hooper_Receipt/src/main/java/com/hooper/controllers/HooperController.java
@@ -0,0 +1,32 @@
+package com.hooper.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HooperController {
+	
+	
+	// class definition and imports here...
+    @RequestMapping("/")
+    public String index(Model model) {
+        
+        String name = "Grace Hopper";
+        model.addAttribute("name", name);
+        String itemName = "Copper wire";
+        model.addAttribute("itemName", itemName);
+        double price = 5.25;
+        model.addAttribute("price", price);
+        String description = "Metal strips, also an illustration of nanoseconds.";
+        model.addAttribute("description", description);
+        String vendor = "Little Things Corner Store";
+        model.addAttribute("vendor", vendor);
+        
+    	// Your code here! Add values to the view model to be rendered
+    
+        return "index.jsp";
+    }
+    //...
+    
+
+}
