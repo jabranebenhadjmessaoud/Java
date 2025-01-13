a5c3c2ee76fde7b01901fb05821a691479f07e39
Jabrane <jabrane.benhadjmessaoud@gmail.com>
2025-01-13 14:08:19 +0100

    counter practice

---
+++ Week2/Day1/Practice/counter/src/main/java/com/counter/models/CounterModel.java
@@ -0,0 +1,18 @@
+package com.counter.models;
+
+public class CounterModel {
+	private int count ;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public CounterModel(int count) {
+		this.count = count;
+	}
+
+}
