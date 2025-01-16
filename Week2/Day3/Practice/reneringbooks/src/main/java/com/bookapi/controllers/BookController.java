287085de9f85918a6eb7b1c3fb489abe8e72c3c0
Jabrane Ben Hadj Messaoud <jabrane.benhadjmessaoud@gmail.com>
2025-01-16 16:21:46 +0100

    rendering books practice

---
+++ Week2/Day3/Practice/reneringbooks/src/main/java/com/bookapi/controllers/BookController.java
@@ -0,0 +1,29 @@
+package com.bookapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.bookapi.services.BookService;
+
+
+@Controller
+public class BookController {
+	@Autowired
+	BookService bookService;
+	
+	@GetMapping("/")
+	public String home() {
+		return "redirect:/books";
+	}
+	
+	@GetMapping("/books/{bookId}")
+	public String index(Model model, @PathVariable("bookId") Long bookId) {
+		
+        model.addAttribute("book", bookService.findBook(bookId));
+        return "show.jsp";  
+	}
+
+}
